1e1b8e3cbe1694be0d24dde2d99647d991940a8c
Lena <wanderfalke@posteo.de>
2024-06-26 16:55:54 +0200

    merged pages challenges_outlook and conclusion=

--- streamlit_app/sites/challenges_outlook.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.header('Challenges')
-
-
-st.header('Outlook')
--- streamlit_app/sites/conclusion.py
+++ streamlit_app/sites/conclusion.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.header('Conclusion')
+st.header('Conclusion & Outlook')
 
 st.write("""
         
