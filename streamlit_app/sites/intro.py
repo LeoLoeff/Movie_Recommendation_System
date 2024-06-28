bf18997bb725afd4168972c8fb003554a253cc06
Lena <wanderfalke@posteo.de>
2024-06-28 17:55:16 +0200

    intro.py: updated; models_classical: added methodology

--- streamlit_app/sites/intro.py
+++ streamlit_app/sites/intro.py
@@ -17,7 +17,10 @@ st.markdown("""
     ## Introduction to the Project
     
     ### Overview
+    - **Collaborative Filtering**: The project employs collaborative filtering, focusing on similarities between users and items respectively to provide recommendations.
     - **Data Analysis and Preprocessing**: Initial data analysis and preprocessing were conducted to create a manageable dataset for further analysis and model building.
     - **Model Evaluation**: Various models were trained and fine-tuned, with the best-performing models evaluated for their effectiveness.
-    - **Collaborative Filtering**: The project employs collaborative filtering, focusing on similarities between users and/or items to provide recommendations.
+    - **Common Challenges**: 
+        - Data Sparcity: Most users interact with only a small subset of items, making it challenging to find enough data to generate reliable recommendations.
+        - Scalability: Computational complexity increases with growing database.
 """, unsafe_allow_html=True)
